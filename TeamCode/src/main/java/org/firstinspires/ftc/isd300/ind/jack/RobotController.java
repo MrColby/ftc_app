04294117f4b591e0dbb7727c9f4e982016c888b3
colbyl <logan.colby@isd300.k12.mn.us>
2017-10-04 16:34:59 -0500

    Sync

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/jack/RobotController.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.isd300.ind.jack;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by colbyl on 10/4/2017.
+ */
+@TeleOp(name="JackRobotController", group="jack")
+public class RobotController extends LinearOpMode {
+    Terminator terminator;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        terminator = new Terminator(this.telemetry);
+        terminator.talk("Info", "hello");
+
+        this.waitForStart();
+
+    }
+}
