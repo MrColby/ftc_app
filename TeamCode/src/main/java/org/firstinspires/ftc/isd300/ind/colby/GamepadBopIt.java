6596ecc127a49b50955357498808ec20ad6cfe30
colbyl <logan.colby@isd300.k12.mn.us>
2017-10-13 16:58:12 -0500

    Sync

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/colby/GamepadBopIt.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.isd300.ind.colby;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by colbyl on 10/7/2017.
+ */
+
+public class GamepadBopIt extends GamepadHandler {
+
+    protected Telemetry telemetry;
+
+
+
+    public GamepadBopIt(Gamepad pad, String i, Telemetry tel) {
+        super(pad, i);
+        this.telemetry = tel;
+    }
+
+
+
+     /* ACT ON GAMEPAD CHANGES*/
+
+    protected void gamepad_x(boolean b) {
+
+    }
+
+    protected void gamepad_y(boolean b) {
+
+    }
+
+    protected void gamepad_a(boolean b) {
+
+    }
+
+    protected void gamepad_b(boolean b) {
+
+    }
+
+    protected void gamepad_start(boolean b) {
+
+    }
+
+    protected void gamepad_back(boolean b) {
+
+    }
+
+    protected void gamepad_dpad_down(boolean b) {
+
+    }
+
+    protected void gamepad_dpad_left (boolean b) {
+
+    }
+
+    protected void gamepad_dpad_up (boolean b) {
+
+    }
+
+    protected void gamepad_dpad_right (boolean b) {
+
+    }
+
+    protected void gamepad_guide (boolean b) {
+
+    }
+
+    protected void gamepad_left_bumper (boolean b) {
+
+    }
+
+    protected void gamepad_left_stick_button (boolean b) {
+
+    }
+
+    protected void gamepad_left_stick(float x, float y) {
+
+    }
+
+    protected void gamepad_left_trigger (float f) {
+
+    }
+
+    protected void gamepad_right_bumper (boolean b) {
+
+    }
+
+    protected void gamepad_right_stick_button (boolean b) {
+
+
+    }
+
+    protected void gamepad_right_stick(float x, float y) {
+
+    }
+
+    protected void gamepad_right_trigger (float f) {
+
+    }
+
+
+
+
+
+
+}
