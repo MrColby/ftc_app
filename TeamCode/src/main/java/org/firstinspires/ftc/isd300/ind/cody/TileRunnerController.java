a070ee72a4095e86b11f4aee5ba0136f6e3ee560
CodyH <cody.helk125@gmail.com>
2017-11-07 18:23:40 -0600

    cody

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/cody/TileRunnerController.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.isd300.ind.cody;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.isd300.ind.jack.TheRobot;
+
+/**
+ * Created by helcod22 on 11/6/2017.
+ */
+
+public class TileRunnerController extends LinearOpMode {
+
+    TheRobot robot;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot = new TheRobot(telemetry, this.hardwareMap);
+        this.waitForStart();
+        while (this.opModeIsActive()) {
+            robot.driveY(this.gamepad1);
+            robot.driveX(this.gamepad1);
+            robot.driveTL(this.gamepad1);
+            robot.drivedTR(this.gamepad1);
+
+        }
+    }
+
+
+
+}
