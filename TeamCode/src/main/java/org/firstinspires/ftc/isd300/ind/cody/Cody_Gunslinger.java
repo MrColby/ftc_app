de5928c10349cb193f2637cf8a248ff6a5c981ed
codyhelke13 <cody.helke125@gmail.com>
2017-11-28 19:58:49 -0600

    Started Gunslinger

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/cody/Cody_Gunslinger.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.isd300.ind.cody;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by helcod22 on 11/28/2017.
+ */
+@TeleOp(name="Cody_gunslinger", group="Dolphin")
+public class Cody_Gunslinger extends LinearOpMode{
+    public void call(String caption, String message){
+        telemetry.addData (caption, message);
+        telemetry.update();
+    }
+    public void runOpMode() throws InterruptedException {
+        boolean playing = true;
+        this.waitForStart();
+        double time = 2000;
+        while (opModeIsActive() && playing ==true) {
+            call ("alert","go!");
+            if (gamepad1.x == true)  {
+                //break;
+                playing = false;
+            }
+        }
+    }
+}
