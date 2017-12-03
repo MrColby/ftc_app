e383c4c0bcd4dc3eaa08905f42f72018aa3141dc
panda <aristayin@gmail.com>
2017-12-03 10:50:38 -0600

    Merge branch 'master' of https://github.com/MrColby/ftc_app
    # Conflicts:
    #	gradle/wrapper/gradle-wrapper.properties

--- TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/alyssa/Alyssa_gunslinger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/alyssa/Alyssa_gunslinger.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.isd300.teamcode;
+package org.firstinspires.ftc.isd300.ind.alyssa;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
