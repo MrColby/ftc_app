337552b87b4fc9a1e41609765da50693978c19ca
panda <aristayin@gmail.com>
2017-12-03 10:50:38 -0600

    Merge branch 'master' of https://github.com/MrColby/ftc_app
    # Conflicts:
    #	gradle/wrapper/gradle-wrapper.properties

--- TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/alyssa/Alyssa_gunslinger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/alyssa/Alyssa_gunslinger.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.isd300.ind.alyssa;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -9,7 +8,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Created by colbyl on 11/28/2017.
  */
 @TeleOp(name="Alyssa_gunslinger", group="sheep")
-@Disabled
 public class Alyssa_gunslinger extends LinearOpMode{
     public void call(String caption, String message) {
         telemetry.addData(caption, message);
