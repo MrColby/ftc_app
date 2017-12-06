57e7def89d8078968e1a4fc893f0e9862103c530
tolaly22 <tollefsonfamily4@gmail.com>
2017-12-05 20:00:28 -0600

    vuforia started

--- TeamCode/src/main/java/org/firstinspires/ftc/isd300/teamcode/Autotamouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/teamcode/Autotamouse.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.isd300.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
 /**
  * Created by whiar on 12/3/2017.
  */
@@ -7,9 +12,17 @@ package org.firstinspires.ftc.isd300.teamcode;
 public class Autotamouse {
     private boolean red;
     private boolean far;
-    public Autotamouse(boolean red, boolean far) {
+    private TotBot totBot;
+    private HardwareMap hardwareMap;
+    private Telemetry telemetry;
+    public Autotamouse(boolean red, boolean far, Telemetry telemetry, HardwareMap hardwareMap) {
         this.red = red;
         this.far = far;
-
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+    }
+    public void runMission() {
+        totBot = new TotBot(this.hardwareMap,this.telemetry);
+        RelicRecoveryVuMark vuMark = totBot.getPictograph();
     }
 }
