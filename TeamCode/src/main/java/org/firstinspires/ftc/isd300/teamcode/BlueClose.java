287d1e80e8871cfd30ae952b5045763d1919c8f8
tolaly22 <tollefsonfamily4@gmail.com>
2017-12-05 20:00:28 -0600

    vuforia started

--- TeamCode/src/main/java/org/firstinspires/ftc/isd300/teamcode/BlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/teamcode/BlueClose.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.isd300.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 /**
  * Created by whiar on 12/3/2017.
  */
@@ -10,10 +12,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class BlueClose extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
-        Autotamouse marmotot =  new Autotamouse(false, false);
+        Autotamouse marmotot =  new Autotamouse(false, false, telemetry, hardwareMap);
         this.waitForStart();
-        //marmotot.run();
-        while (this.opModeIsActive()){
+        marmotot.runMission();
+        while (this.opModeIsActive()) {
+
         }
 
     }
