961dada3d43a025c78bbf838494911a2a945dee9
colbyl <logan.colby@isd300.k12.mn.us>
2017-12-12 20:12:13 -0600

    Sync

--- TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/jack/mecanumtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/isd300/ind/jack/mecanumtest.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.isd300.ind.jack;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.isd300.ind.jacob.*;
-
 /**
  * Created by colbyl on 10/17/2017.
  */
